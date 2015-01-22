d38ac5efe053403587bdc1d059cc513c40352e5a
Erin Yaylali <erinyaylali@gmail.com>
2015-01-22 00:04:24 -0800

    Working v1

--- markov.py
+++ markov.py
@@ -20,50 +20,50 @@ def make_chains(word_bank_list):
     markov chains."""
 
 
-    markov_chain_dict = {}
+    markov_dict_final = {}
 
-    for i in range(len(word_bank_list)):
-        if word_bank_list[i] == word_bank_list[-1]:
-            continue
+    for i in range(len(word_bank_list)-2):
+        key = tuple([word_bank_list[i],word_bank_list[i+1]])
+
+        if key not in markov_dict_final:
+            markov_dict_final[key] = [word_bank_list[i+2]]
         else:
-            tupled = tuple([word_bank_list[i],word_bank_list[i+1]])
-            if tupled in markov_chain_dict:
-                markov_chain_dict[tupled] = markov_chain_dict[tupled] + [word_bank_list[i+2]]
-                #this is where we'll add value after it to the list value of the existing one
-            else:
-                if word_bank_list[i+1] == word_bank_list[-1]:
-                    continue
-                else:
-                    markov_chain_dict[tupled] = [word_bank_list[i+2]]
+            markov_dict_final[key] = markov_dict_final[key] + [word_bank_list[i+2]]
 
-    return markov_chain_dict
+    return markov_dict_final
 
 
 def make_text(markov_dict_final):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    for element in markov_dict_final:
+    random_key = random.choice(markov_dict_final.keys())
+    tuple_at_0 = random_key[1]
+    tuple_at_1 = random.choice(markov_dict_final[random_key])
+    punctuation = ["?", ".", "!"]
+    markov_text = [tuple_at_0,tuple_at_1] #these could have punctuation. check for them. how should we control for capitalization?
 
-        random_key = random.choice(markov_dict_final.keys())
-        tuple1 = random_key[1]
-        tuple2 = random.choice(markov_dict_final[random_key])
-        
-    return "%s %s" % (tuple1,tuple2)
+    while True:
+        search_tuple = (markov_text[-2],markov_text[-1])
+        if search_tuple in markov_dict_final:
+            next_word_options = markov_dict_final[search_tuple]
+            next_word = random.choice(next_word_options)
+            markov_text.append(next_word)
+            if next_word[-1] in punctuation:
+                break
+        else:
+            break
+    
+    markov_output = " ".join(markov_text)
+    return markov_output
+    
 
 def main(input_file):
     word_bank_list = unpack_file(input_file)
     markov_dict_final = make_chains(word_bank_list)
     returned_text = make_text(markov_dict_final)
     print returned_text
-#     # args = sys.argv
-
-#     # Change this to read input_text from a file
-#     input_text = "Some text"
-
-#     chain_dict = make_chains(input_text)
-#     random_text = make_text(chain_dict)
-#     print random_text
+ 
 
 if __name__ == "__main__":
     input_file = argv[1]
